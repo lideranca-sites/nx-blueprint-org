f99b53defa79ef1ddd6528671040ab4f0c926655
Mateus Macedo Dos Anjos <mmanjos.mazzatech@lidercap.com.br>
2025-10-26 22:33:16 -0300

    ✨ feat(utils-nestjs): add initial library structure and configuration files

---
+++ libs/utils-nestjs/jest.config.ts
@@ -0,0 +1,10 @@
+export default {
+  displayName: '@lideranca-sites/utils-nestjs',
+  preset: '../../jest.preset.js',
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': ['ts-jest', { tsconfig: '<rootDir>/tsconfig.spec.json' }]
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/libs/utils-nestjs'
+};
